c6f657ea3b9b3d6845094122a589a3f996bb0f70
Edwin Ortega <edwin.maurizio.ortega@gmail.com>
2022-10-05 23:04:45 -0500

    menkuff

--- pages/pages/imei.jsx
+++ pages/pages/imei.jsx
@@ -5,6 +5,7 @@ import {useLazyQuery} from "@apollo/react-hooks";
 
 import {GET_GARANTIA_IMEI_DATA} from "/server/queries";
 import withApollo from "server/apollo";
+import ALink from "~/components/features/custom-link";
 
 function SearchIMEI() {
 
@@ -30,7 +31,7 @@ function SearchIMEI() {
             <div className="page-content">
                 <section
                     className="error-section d-flex flex-column justify-content-center align-items-center text-center pl-3 pr-3">
-                    <h1 className="mb-2 ls-m">Validar Garantia Lidenar - Hipertronics</h1>
+                    <h1 className="mb-2 ls-m">Validar Garantía Lidenar - Hipertronics</h1>
                     <div className="App" style={{display: "contents"}}>
 
                         <div className="input-wrapper input-wrapper-inline input-wrapper-round">
@@ -51,6 +52,38 @@ function SearchIMEI() {
 
 
                 </section>
+                <div className="container mt-12">
+                <form action="#" className="form">
+                    <div className="row">
+                        <div className="col-lg-12 mb-6 mb-lg-0 pr-lg-4">
+                            <h3 className="title title-simple text-left text-uppercase">CREAR ORDEN - EQUIPO FUERA DE GARANTÍA</h3>
+                            <div className="row">
+                                <div className="col-xs-6">
+                                    <label>IMEI *</label>
+                                    <input type="text" className="form-control" name="first-name" required />
+                                </div>
+                                <div className="col-xs-6">
+                                    <label>FECHA FIN GARANTÍA *</label>
+                                    <input type="text" className="form-control" name="first-name" required />
+                                </div>
+                                <div className="col-xs-6">
+                                    <label>NÚMERO DE CONTACTO *</label>
+                                    <input type="text" className="form-control" name="last-name" required />
+                                </div>
+                            </div>
+                            <label>FALLAS DEL EQUIIPO *</label>
+                            <textarea className="form-control pb-2 pt-2 mb-0" cols="30" rows="5"
+                                      placeholder="Diagnóstico de equipo celular." required></textarea>
+
+                            <div className="form-checkbox mt-4 mb-5">
+                                <button type="submit" className="btn btn-dark btn-rounded btn-order">CREAR - IMPRIMIR</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </form>
+                </div>
             </div>
         </main>
     )
